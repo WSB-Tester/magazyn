fbf89528df6db4a05858883ddaa939e5d1d139d8
Maniek <emailer5k@gmail.com>
2019-10-27 17:35:19 +0100

    Add content to branch

---
+++ main.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+# name = input(str)
+
+# if name == "Maniek":
+#     print ("Hello World! This is my name: " + name)
+# else:
+#      print("please provide a correct username")
+
+# name = "Maniek"
+# cars_count = 10
+# avg = 2.2
+
+# print("hello " + name)
+# print(name + "has " + str(cars_count) + " cars")
+# print("Average engine capacity: " + str(avg))
+
+# brand = "Peugeot"
+# doors = 5 
+# eng_cap = 1.3
+
+# brand_up = brand.upper()
+# brand_low = brand.lower()
+
+# print("Car " + brand + " has " + str(doors) + " drzwi")
+# print(brand_up)
+# print(brand_low)
+# print("Engine capacity after changes " + str(eng_cap *2))
+
+
+# input = input()
+# pwd = len(input)
+# inputlen = len(input)
+# ast = str((inputlen - 2) * '*')
+
+
+# if pwd == 0:
+#     print("password empty")
+# elif pwd < 3:
+#     print("password too short")
+# else:
+#     result = input[:1] + ast + input[-1]
+#     print(result)
+
+# haslo = input()
+# pwd = len(haslo)
+
+# if pwd == 0:
+#     print("password empty")
+# elif pwd < 3:
+#     print("password too short")
+# else:
+#     for i in range(1, pwd -1):
+#         result = haslo[0] + (pwd-2)*'*' + haslo[-1]
+#         print(result)
+#         break
+
+# def print_dict(d):
+#     for key, value in samolot.iteritems():
+#         print("{0}:{1}".format(key, value))
+
+# if __name__ == "__main__":
+#     samolot = {'name': 'boeing',
+#         'przebieg': 10000,
+#         'type': 'pasazerski'}
+
+
+# def calculate_vat(netto):
+#     vat = float(netto * 23)/100
+#     return vat
+
+# if __name__ == "__main__":
+#     vat = calculate_vat(1000)
+    
+
+# def main():
+#     print_dict(samolot)
+#     print("{0}".format(vat))
+
+# if __name__ == "__main__":
+#     main()
+
+# cars = ['syrena', 'warszawa', 'polonez', 'fiat']
+# count = [3, 5]
+
+# for s in cars:
+#     print(s)
+
+# for idx in range(len(cars)):
+#     print("idx: " + str(idx) + " : " + cars[idx])
+#     print(cars[idx] + " liczba drzwi: " + str(count[idx]))
+
+import pprint
+
+# def function():
+#     s = {"jedno": "ala ma kota",
+#         "drugie": [1,2,3,4],
+#         "trzecie": "wiele halasu o",
+#         "czwarte": range(5,15)}
+#     file = open("testfile.csv", "a")
+#     file.write(str(s))
+#     file.close()
+
+#     with open("testfile.csv", "r") as f:
+#         read = f.read()
+#         print(read)
+
+#         if len(read) > 1000:
+#             file = open("testfile.csv", "w")
+#             file.close()
+
+
+# if __name__ == "__main__":
+#     function()
